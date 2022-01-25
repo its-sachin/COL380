6ae1babf927b351fb5b0026aaa8f7f5fd7e960ba
its-sachin <sachinsain28786@gmail.com>
2022-01-25 22:18:25 +0530

    added A1 base files

---
+++ A1/psort.cpp
@@ -0,0 +1,9 @@
+#include "psort.h"
+#include <omp.h>
+
+
+void ParallelSort(uint32_t *data, uint32_t n, int p)
+{
+    // Entry point to your sorting implementation.
+    // Sorted array should be present at location pointed to by data.
+}
